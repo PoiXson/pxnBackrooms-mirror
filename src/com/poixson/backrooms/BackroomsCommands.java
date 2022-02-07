63aca1498c8aef3e56d9b62ee367c17b07f4f547
lorenzop <lorenzo@poixson.com>
2022-02-07 01:57:17 -0500

    added commands class

---
+++ src/com/poixson/backrooms/BackroomsCommands.java
@@ -0,0 +1,39 @@
+package com.poixson.backrooms;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+
+import com.poixson.common.tools.commands.pxnBukkitCommand;
+import com.poixson.common.tools.commands.pxnBukkitCommandsHelper;
+
+
+public class BackroomsCommands extends pxnBukkitCommandsHelper {
+
+
+
+	public BackroomsCommands(final BackroomsPlugin plugin) {
+		super(plugin, "backrooms");
+		// /backrooms reload
+		this.addCommand(new cmdReload());
+	}
+
+
+
+	// /worldsq reload
+	public static class cmdReload extends pxnBukkitCommand {
+		public cmdReload() {
+			super("reload");
+		}
+		@Override
+		public boolean run(final CommandSender sender,
+				final Command cmd, final String[] args) {
+			
+BackroomsPlugin.log.warning("RELOAD COMMAND");
+			
+			return true;
+		}
+	}
+
+
+
+}
