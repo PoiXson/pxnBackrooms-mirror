3418f6b76c9b1d12c8158cd87e5692cc811f2463
lorenzop <lorenzo@poixson.com>
2022-12-08 01:05:35 -0500

    added getLevel() getBackroomsWorld() and noclip() functions to BackroomsPlugin class

--- src/com/poixson/backrooms/BackroomsPlugin.java
+++ src/com/poixson/backrooms/BackroomsPlugin.java
@@ -2,12 +2,16 @@ package com.poixson.backrooms;
 
 import java.io.File;
 import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map.Entry;
 import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.atomic.AtomicReference;
 import java.util.logging.Logger;
 
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
+import org.bukkit.World;
+import org.bukkit.entity.Player;
 import org.bukkit.event.HandlerList;
 import org.bukkit.generator.ChunkGenerator;
 import org.bukkit.plugin.java.JavaPlugin;
@@ -16,6 +20,7 @@ import com.poixson.backrooms.generators.BackGen_000;
 import com.poixson.backrooms.generators.BackGen_009;
 import com.poixson.backrooms.generators.BackGen_011;
 import com.poixson.backrooms.generators.BackroomsGenerator;
+import com.poixson.utils.NumberUtils;
 
 
 public class BackroomsPlugin extends JavaPlugin {
@@ -95,6 +100,95 @@ public class BackroomsPlugin extends JavaPlugin {
 
 
 
+	public int getLevel(final Player player) {
+		if (player == null)
+			return Integer.MIN_VALUE;
+		final World world = player.getWorld();
+		return this.getLevel(world);
+	}
+	public int getLevel(final World world) {
+		if (world == null)
+			return Integer.MIN_VALUE;
+		return this.getLevel(world.getName());
+	}
+	public int getLevel(final String worldName) {
+		if (worldName == null || worldName.isEmpty())
+			return Integer.MIN_VALUE;
+		if (!worldName.startsWith("level"))
+			return Integer.MIN_VALUE;
+		return Integer.parseInt(worldName.substring(5));
+	}
+
+
+
+	public int noclip(final int from) {
+		if (from == Integer.MIN_VALUE)
+			return 0;
+		final HashMap<Integer, Integer> chance = new HashMap<Integer, Integer>();
+		switch (from) {
+		case 0: // lobby
+			chance.put(Integer.valueOf(  -1 ), Integer.valueOf( 25 )); // basement
+			chance.put(Integer.valueOf(   5 ), Integer.valueOf( 15 )); // hotel
+			chance.put(Integer.valueOf(   9 ), Integer.valueOf( 10 )); // suburbs
+			chance.put(Integer.valueOf(  11 ), Integer.valueOf( 10 )); // city
+			chance.put(Integer.valueOf( 309 ), Integer.valueOf( 10 )); // path
+			break;
+		case -1: // basement
+			chance.put(Integer.valueOf(   0 ), Integer.valueOf( 20 )); // lobby
+			chance.put(Integer.valueOf(   5 ), Integer.valueOf(  2 )); // hotel
+			chance.put(Integer.valueOf(   9 ), Integer.valueOf( 10 )); // suburbs
+			chance.put(Integer.valueOf(  11 ), Integer.valueOf( 10 )); // city
+		case 5: // hotel
+			chance.put(Integer.valueOf(   0 ), Integer.valueOf( 20 )); // lobby
+			chance.put(Integer.valueOf(  -1 ), Integer.valueOf( 15 )); // basement
+			chance.put(Integer.valueOf(  11 ), Integer.valueOf( 10 )); // city
+			chance.put(Integer.valueOf( 309 ), Integer.valueOf( 10 )); // path
+		case 9: // suburbs
+			chance.put(Integer.valueOf(   0 ), Integer.valueOf(  5 )); // lobby
+			chance.put(Integer.valueOf(  11 ), Integer.valueOf( 10 )); // city
+			chance.put(Integer.valueOf( 309 ), Integer.valueOf(  5 )); // path
+		case 11: // city
+			chance.put(Integer.valueOf(   0 ), Integer.valueOf(  5 )); // lobby
+			chance.put(Integer.valueOf(  -1 ), Integer.valueOf( 10 )); // basement
+			chance.put(Integer.valueOf(   5 ), Integer.valueOf( 10 )); // hotel
+			chance.put(Integer.valueOf(   9 ), Integer.valueOf(  5 )); // suburbs
+			chance.put(Integer.valueOf( 309 ), Integer.valueOf( 10 )); // path
+		case 309: // path
+			chance.put(Integer.valueOf(   0 ), Integer.valueOf( 10 )); // lobby
+			chance.put(Integer.valueOf(   9 ), Integer.valueOf( 15 )); // suburbs
+			chance.put(Integer.valueOf(  11 ), Integer.valueOf( 10 )); // city
+		}
+		if (chance.isEmpty())
+			return 0;
+		int total = 0;
+		for (final Integer i : chance.keySet()) {
+			total += i;
+		}
+		final int rnd = NumberUtils.GetRandom(0, total);
+		final Iterator<Entry<Integer, Integer>> it = chance.entrySet().iterator();
+		while (it.hasNext()) {
+			final Entry<Integer, Integer> entry = it.next();
+			final Integer level  = entry.getKey();
+			final Integer weight = entry.getValue();
+			if (total - weight <= rnd)
+				return level.intValue();
+			total -= weight;
+		}
+		return 0;
+	}
+
+
+
+	public World getBackroomsWorld(final int level) {
+		switch (level) {
+		case 9:  return Bukkit.getWorld("level9");
+		case 11: return Bukkit.getWorld("level11");
+		}
+		return Bukkit.getWorld("level0");
+	}
+
+
+
 	// -------------------------------------------------------------------------------
 
 
@@ -105,7 +199,7 @@ public class BackroomsPlugin extends JavaPlugin {
 			throw new RuntimeException("Invalid world name, must be level#");
 		log.info(String.format("%sWorld <%s> using generator <%s> %s",
 			LOG_PREFIX, worldName, GENERATOR_NAME, argsStr));
-		final int level = Integer.parseInt(worldName.substring(5));
+		final int level = this.getLevel(worldName);
 		// existing generator
 		{
 			final BackroomsGenerator gen = this.generators.get(Integer.valueOf(level));
