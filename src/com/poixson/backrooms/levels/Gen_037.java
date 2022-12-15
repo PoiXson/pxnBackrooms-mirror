94b3dc4b1b1f5bec99c1e8f89e08fdd752781e91
lorenzop <lorenzo@poixson.com>
2022-12-15 08:17:57 -0500

    added subfloor and ceilings to poolrooms

--- src/com/poixson/backrooms/levels/Gen_037.java
+++ src/com/poixson/backrooms/levels/Gen_037.java
@@ -1,5 +1,6 @@
 package com.poixson.backrooms.levels;
 
+import org.bukkit.Material;
 import org.bukkit.generator.ChunkGenerator.ChunkData;
 
 
@@ -12,6 +13,10 @@ public class Gen_037 extends BackroomsGenerator {
 
 	public static final int POOLS_Y = Level_000.Y_037;
 	public static final int POOLS_H = Level_000.H_037;
+
+	public static final Material POOLS_WALL       = Material.DARK_PRISMARINE;
+	public static final Material POOLS_SUBFLOOR   = Material.DARK_PRISMARINE;
+	public static final Material POOLS_SUBCEILING = Material.DARK_PRISMARINE;
 
 
 
@@ -32,6 +37,22 @@ public class Gen_037 extends BackroomsGenerator {
 
 	public void generatePools(final int chunkX, final int chunkZ, final ChunkData chunk,
 			final int x, final int z, final int xx, final int zz) {
+		int y  = POOLS_Y;
+		int cy = POOLS_Y + SUBFLOOR + POOLS_H;
+		// lobby floor
+		chunk.setBlock(x, y, z, Material.BEDROCK);
+		y++;
+		for (int yy=0; yy<SUBFLOOR; yy++) {
+			chunk.setBlock(x, y+yy, z, POOLS_SUBFLOOR);
+		}
+		y += SUBFLOOR;
+//TODO
+		if (BUILD_ROOF) {
+			cy++;
+			for (int i=0; i<SUBCEILING; i++) {
+				chunk.setBlock(x, cy+i, z, POOLS_SUBCEILING);
+			}
+		}
 	}
 
 
