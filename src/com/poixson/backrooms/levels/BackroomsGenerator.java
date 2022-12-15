529d22dc026d2cc22bb828d9a6c018d0b43c7c78
lorenzop <lorenzo@poixson.com>
2022-12-15 05:29:02 -0500

    added BackroomsGenerator class - other class was renamed to BackroomsLevel

---
+++ src/com/poixson/backrooms/levels/BackroomsGenerator.java
@@ -0,0 +1,19 @@
+package com.poixson.backrooms.levels;
+
+
+public abstract class BackroomsGenerator {
+
+
+
+	public BackroomsGenerator() {
+	}
+	public void unload() {
+	}
+
+
+
+	public abstract void setSeed(final int seed);
+
+
+
+}
