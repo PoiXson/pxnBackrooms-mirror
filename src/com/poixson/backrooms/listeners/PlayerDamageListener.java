40a6aa5797e66e300ec6ca99145f922fb149bbec
lorenzop <lorenzo@poixson.com>
2022-12-16 04:16:16 -0500

    finished and improved PlayerDamageListener

--- src/com/poixson/backrooms/listeners/PlayerDamageListener.java
+++ src/com/poixson/backrooms/listeners/PlayerDamageListener.java
@@ -1,21 +1,51 @@
 package com.poixson.backrooms.listeners;
 
-import java.util.concurrent.ConcurrentHashMap;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map.Entry;
+import java.util.UUID;
 import java.util.concurrent.atomic.AtomicInteger;
 
+import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDamageEvent;
+import org.bukkit.event.entity.EntityDamageEvent.DamageCause;
 
 import com.poixson.backrooms.BackroomsPlugin;
+import com.poixson.tools.xTime;
+import com.poixson.utils.Utils;
 
 
 public class PlayerDamageListener implements Listener {
 
+	public static final long DAMAGE_TIMEOUT = xTime.Parse("5s").ms();
+	public static final double MIN_DAMAGE = 3.0;
+
 	protected final BackroomsPlugin plugin;
 
-	protected final ConcurrentHashMap<Player, Long> lastDamaged = new ConcurrentHashMap<Player, Long>();
-
 	protected final AtomicInteger cleanup = new AtomicInteger(0);
+
+
+
+	public class PlayerDamageDAO {
+		public long last;
+		public int count = 1;
+
+		public PlayerDamageDAO() {
+			this.last = Utils.GetMS();
+		}
+
+		public int increment() {
+			this.last = Utils.GetMS();
+			return ++this.count;
+		}
+
+	}
+	protected final HashMap<UUID, PlayerDamageDAO> lastPlayerDamage = new HashMap<UUID, PlayerDamageDAO>();
 
 
 
@@ -25,89 +55,69 @@ public class PlayerDamageListener implements Listener {
 
 
 
-/*
 	@EventHandler(priority=EventPriority.NORMAL)
 	public void onPlayerDamage(final EntityDamageEvent event) {
 		final Entity entity = event.getEntity();
 		if (entity instanceof Player) {
 			final Player player = (Player) entity;
+			final double health = player.getHealth();
 			final DamageCause cause = event.getCause();
-			if (cause.compareTo(DamageCause.DROWNING)    == 0
-			||  cause.compareTo(DamageCause.SUFFOCATION) == 0) {
-				final long current = Utils.GetMS();
-				// cleanup
-				if (this.cleanup.incrementAndGet() > 20) {
-					this.cleanup.set(0);
-					final Iterator<Entry<Player, Long>> it = this.lastDamaged.entrySet().iterator();
-					final HashSet<Player> remove = new HashSet<Player>();
-					while (it.hasNext()) {
-						final Entry<Player, Long> entry = it.next();
-						if (current - entry.getValue() > 300000L)
-							remove.add(entry.getKey());
-					}
-					for (final Player p : remove) {
-						this.lastDamaged.remove(p);
-					}
+			switch (cause) {
+			case SUFFOCATION:
+			case DROWNING:
+			case POISON:
+			case VOID: {
+				final int count = this.incrementPlayerDamageCount(player.getUniqueId());
+				final int countMin = (health > MIN_DAMAGE ? 4 : 2);
+				if (count <= countMin)
+					return;
+				break;
+			}
+			case FLY_INTO_WALL:
+			case FALL: break;
+			default: return;
+			}
+			final int rnd = BackroomsPlugin.Rnd10K();
+			if (rnd % 10 < 5)
+				return;
+			// noclip into the backrooms
+			event.setCancelled(true);
+			this.plugin.noclip(player);
+			this.lastPlayerDamage.remove(player.getUniqueId());
+			// cleanup
+			if (this.lastPlayerDamage.size() % 5 == 0) {
+				final long time = Utils.GetMS();
+				final ArrayList<UUID> remove = new ArrayList<UUID>();
+				final Iterator<Entry<UUID, PlayerDamageDAO>> it = this.lastPlayerDamage.entrySet().iterator();
+				while (it.hasNext()) {
+					final Entry<UUID, PlayerDamageDAO> entry = it.next();
+					final PlayerDamageDAO dao = entry.getValue();
+					if (dao.last + DAMAGE_TIMEOUT > time)
+						remove.add(entry.getKey());
 				}
-				// no-clip player to the backrooms
-				boolean tp = false;
-				final double damage = event.getFinalDamage();
-				final double health = player.getHealth();
-				// prevent death
-				if (health - damage <= 2.0) {
-					event.setCancelled(true);
-					tp = true;
+				for (final UUID uuid : remove) {
+					this.lastPlayerDamage.remove(uuid);
 				}
-				if (!tp) {
-					final Long lastDam = this.lastDamaged.get(player);
-					if (lastDam != null) {
-						final long last = lastDam.longValue();
-						// damaged within last 5 seconds
-						if (current - last < 5000L) {
-							// random chance
-							double chance = 8.0;
-							if (cause.compareTo(DamageCause.SUFFOCATION) == 0)
-								chance = 2.5;
-							int rnd = 0;
-							for (int i=0; i<5; i++) {
-								rnd = NumberUtils.GetNewRandom(0, 9000, rnd);
-							}
-							final double mod = ((double)rnd) % chance;
-							if (mod < 1.0)
-								tp = true;
-						}
-					} // end lastDam
-				} // end !tp
-				if (tp) {
-					event.setCancelled(true);
-					this.tp(player);
-				}
-			} // end damage cause
-		} // end player instance
-	}
-
-
-
-	protected void tp(final Player player) {
-		final String worldName = this.plugin.getBackroomsWorldName();
-		final World world = Bukkit.getWorld(worldName);
-		if (world == null) {
-			player.sendMessage(BackroomsPlugin.CHAT_PREFIX + "The Backrooms could not be found.");
-			return;
+			}
 		}
-//TODO: remove this
-for (int i=0; i<10; i++) {
-	final Location sp = world.getSpawnLocation();
-	System.out.println();
-	System.out.println();
-	System.out.println("SPAWN x: " + sp.getX() + " y: " + sp.getY() + " z: " + sp.getZ());
-	System.out.println();
-	System.out.println();
-}
-		final Location spawn = world.getSpawnLocation();
-		player.teleport(spawn);
 	}
-*/
+
+	public int incrementPlayerDamageCount(final UUID uuid) {
+		// cached
+		{
+			final PlayerDamageDAO dao = this.lastPlayerDamage.get(uuid);
+			if (dao != null) {
+				if (dao.last + DAMAGE_TIMEOUT >= Utils.GetMS())
+					return dao.increment();
+			}
+		}
+		// new dao
+		{
+			final PlayerDamageDAO dao = new PlayerDamageDAO();
+			this.lastPlayerDamage.put(uuid, dao);
+			return dao.count;
+		}
+	}
 
 
 
