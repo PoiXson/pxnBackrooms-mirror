a16e399c677fbaaee4a4926dccbc4f7e04b0110a
lorenzop <lorenzo@poixson.com>
2022-12-18 09:21:49 -0500

    started work on level 866 dirtfield

--- src/com/poixson/backrooms/levels/Level_866.java
+++ src/com/poixson/backrooms/levels/Level_866.java
@@ -15,14 +15,23 @@ public class Level_866 extends BackroomsLevel {
 	public static final int DIRTFIELD_Y = 0;
 	public static final int SUBFLOOR = 3;
 
+	public static final Material GROUND      = Material.RED_SANDSTONE;
+	public static final Material GROUND_SLAB = Material.RED_SANDSTONE_SLAB;
+
+	// generators
+	public final Gen_866 gen_866;
+
 
 
 	public Level_866(final BackroomsPlugin plugin) {
 		super(plugin);
+		// generators
+		this.gen_866 = new Gen_866();
 	}
 
 	@Override
 	public void unload() {
+		this.gen_866.unload();
 	}
 
 
@@ -56,12 +65,14 @@ public class Level_866 extends BackroomsLevel {
 	public void generateSurface(
 			final WorldInfo worldInfo, final Random random,
 			final int chunkX, final int chunkZ, final ChunkData chunk) {
-//if (chunkX == 2 && chunkZ == 2) return;
-//if (chunkX % 20 == 0 || chunkZ % 20 == 0) return;
+if (chunkX == 2 && chunkZ == 2) return;
+if (chunkX % 20 == 0 || chunkZ % 20 == 0) return;
+		int xx, zz;
 		for (int z=0; z<16; z++) {
+			zz = (chunkZ * 16) + z;
 			for (int x=0; x<16; x++) {
-				chunk.setBlock(x, 0, z, Material.BEDROCK);
-				chunk.setBlock(x, 1, z, Material.STONE);
+				xx = (chunkX * 16) + x;
+				this.gen_866.generateField(chunkX, chunkZ, chunk, x, z, xx, zz);
 			}
 		}
 	}
