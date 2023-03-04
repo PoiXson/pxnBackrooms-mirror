218a3009d5182d2f810b6a327c796c5fc0db0066
lorenzop <lorenzo@poixson.com>
2023-03-04 06:11:57 -0500

    added dependency for FoodRot plugin

--- build-resource-pack.sh
+++ build-resource-pack.sh
@@ -7,13 +7,13 @@ fi
 
 
 \pushd  "resources/pack/"  >/dev/null  || exit 1
-	\zip -r -9  ../pxnBackrooms-resourcepack.zip *  \
-			|| exit 1
+	\zip -r -9  ../pxnBackrooms-resourcepack.zip *  || exit 1
+\popd >/dev/null
+\pushd  "../FoodRot/resources/pack/"  >/dev/null  || exit 1
+	\zip -r -9  ../../../pxnBackrooms/resources/pxnBackrooms-resourcepack.zip *  || exit 1
 \popd >/dev/null
 
 
 \pushd  "resources/"  >/dev/null  || exit 1
-	\sha1sum pxnBackrooms-resourcepack.zip \
-		> pxnBackrooms-resourcepack.sha1   \
-			|| exit 1
+	\sha1sum pxnBackrooms-resourcepack.zip > pxnBackrooms-resourcepack.sha1  || exit 1
 \popd >/dev/null
