de92c5bd5f16b67c8cf0320ef736e05dc0ffb4ce
lorenzop <lorenzo@poixson.com>
2023-05-02 04:45:17 -0400

    fixed paths in build-resource-pack.sh script

--- build-resource-pack.sh
+++ build-resource-pack.sh
@@ -1,20 +1,25 @@
 #!/usr/bin/bash
 
 
-if [[ -f resources/pxnBackrooms-resourcepack.zip ]]; then
-	\rm -fv  resources/pxnBackrooms-resourcepack.zip  || exit 1
+if [[ -f pxnBackrooms-resourcepack.zip ]]; then
+	\rm -fv  pxnBackrooms-resourcepack.zip  || exit 1
 fi
 
 
-\pushd  "resources/pack/"  >/dev/null  || exit 1
+# backrooms files
+\pushd  "resourcepack/"  >/dev/null  || exit 1
 	\zip -r -9  ../pxnBackrooms-resourcepack.zip *  || exit 1
 \popd >/dev/null
-\pushd  "../FoodRot/resourcepack/"  >/dev/null  || exit 1
-	\zip -r -9  ../../pxnBackrooms/resources/pxnBackrooms-resourcepack.zip *  \
+
+# more foods
+\pushd  "../MoreFoods/resourcepack/"  >/dev/null  || exit 1
+	\zip -r -9  ../../pxnBackrooms/pxnBackrooms-resourcepack.zip *  \
 		--exclude pack.mcmeta  || exit 1
 \popd >/dev/null
 
 
-\pushd  "resources/"  >/dev/null  || exit 1
-	\sha1sum pxnBackrooms-resourcepack.zip > pxnBackrooms-resourcepack.sha1  || exit 1
-\popd >/dev/null
+\sha1sum pxnBackrooms-resourcepack.zip > pxnBackrooms-resourcepack.sha1  || exit 1
+
+
+\cp  pxnBackrooms-resourcepack.zip   resources/
+\cp  pxnBackrooms-resourcepack.sha1  resources/
