6df73abc2ad9dff9442b3bd8d014c38fa3fdd9cb
lorenzop <lorenzo@poixson.com>
2023-05-21 15:56:34 -0400

    added outside of world listener to level 771

--- src/com/poixson/backrooms/listeners/Listener_771.java
+++ src/com/poixson/backrooms/listeners/Listener_771.java
@@ -1,6 +1,11 @@
 package com.poixson.backrooms.listeners;
 
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+
 import com.poixson.backrooms.BackroomsPlugin;
+import com.poixson.commonmc.events.OutsideOfWorldEvent;
 import com.poixson.commonmc.tools.plugin.xListener;
 
 
@@ -11,10 +16,6 @@ public class Listener_771 extends xListener<BackroomsPlugin> {
 	public Listener_771(final BackroomsPlugin plugin) {
 		super(plugin);
 	}
-
-
-
-
 
 
 
@@ -31,9 +32,21 @@ itemB.setItemMeta(potion);
 
 
 
+	// -------------------------------------------------------------------------------
+	// void/sky teleport
 
 
 
+	@EventHandler(priority=EventPriority.NORMAL, ignoreCancelled=true)
+	public void onOutsideOfWorld(final OutsideOfWorldEvent event) {
+		final Player player = event.getPlayer();
+		switch (event.getOutsideWhere()) {
+		case SKY:  this.plugin.noclip(player,  78); break; // to space
+//TODO: teleport relative to basement exit
+		case VOID: this.plugin.noclip(player, 771); break; // to crossroads
+		default: throw new RuntimeException("Unknown OutsideOfWorld event type");
+		}
+	}
 
 
 
