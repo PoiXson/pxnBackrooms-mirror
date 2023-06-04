e7d8fd7645e078f3d0ba49c72c8ee7a3bb4f0115
lorenzop <lorenzo@poixson.com>
2023-06-04 00:28:14 -0400

    fixed and improved build-resource-pack.sh script

--- build-resource-pack.sh
+++ build-resource-pack.sh
@@ -1,25 +1,49 @@
 #!/usr/bin/bash
+VERSION="{{{VERSION}}}"
 
 
-if [[ -f pxnBackrooms-resourcepack.zip ]]; then
-	\rm -fv  pxnBackrooms-resourcepack.zip  || exit 1
+if [[ -z $VERSION ]] || [[ "$VERSION" == "\{\{\{VERSION\}\}\}" ]]; then
+	VERSION=""
+else
+	VERSION="-${VERSION}"
+fi
+
+
+\ls "./pxnBackrooms-resourcepack"*.zip >/dev/null 2>/dev/null
+if [[ $? -eq 0 ]]; then
+	\rm -fv --preserve-root  "./pxnBackrooms-resourcepack"*.zip  || exit 1
+fi
+\ls "./pxnBackrooms-resourcepack"*.sha1 >/dev/null 2>/dev/null
+if [[ $? -eq 0 ]]; then
+	\rm -fv --preserve-root  "./pxnBackrooms-resourcepack"*.sha1  || exit 1
+fi
+if [[ -f "./plugin/resources/pxnBackrooms-resourcepack.zip" ]]; then
+	\rm -fv --preserve-root  "./plugin/resources/pxnBackrooms-resourcepack.zip"  || exit 1
+fi
+if [[ -f "./plugin/resources/pxnBackrooms-resourcepack.sha1" ]]; then
+	\rm -fv --preserve-root  "./plugin/resources/pxnBackrooms-resourcepack.sha1"  || exit 1
 fi
 
 
 # backrooms files
 \pushd  "resourcepack/"  >/dev/null  || exit 1
-	\zip -r -9  ../pxnBackrooms-resourcepack.zip *  || exit 1
+	\zip -r -9  "../pxnBackrooms-resourcepack${VERSION}.zip"  *  || exit 1
 \popd >/dev/null
-
 # more foods
 \pushd  "../MoreFoods/resourcepack/"  >/dev/null  || exit 1
-	\zip -r -9  ../../pxnBackrooms/pxnBackrooms-resourcepack.zip *  \
+	\zip -r -9  "../../pxnBackrooms/pxnBackrooms-resourcepack${VERSION}.zip"  *  \
+		--exclude pack.mcmeta  || exit 1
+\popd >/dev/null
+# redstone terminal
+\pushd  "../RedstoneTerminal/resourcepack/"  >/dev/null  || exit 1
+	\zip -r -9  "../../pxnBackrooms/pxnBackrooms-resourcepack${VERSION}.zip"  *  \
 		--exclude pack.mcmeta  || exit 1
 \popd >/dev/null
 
 
-\sha1sum pxnBackrooms-resourcepack.zip > pxnBackrooms-resourcepack.sha1  || exit 1
+\sha1sum  "pxnBackrooms-resourcepack${VERSION}.zip" \
+	> "pxnBackrooms-resourcepack${VERSION}.sha1"  || exit 1
 
 
-\cp  pxnBackrooms-resourcepack.zip   resources/
-\cp  pxnBackrooms-resourcepack.sha1  resources/
+\cp  "pxnBackrooms-resourcepack${VERSION}.zip"   "resources/pxnBackrooms-resourcepack.zip"
+\cp  "pxnBackrooms-resourcepack${VERSION}.sha1"  "resources/pxnBackrooms-resourcepack.sha1"
