e0a65b28a05a4b47dc7495513dd629d02d70fe26
lorenzop <lorenzo@poixson.com>
2023-06-04 01:15:58 -0400

    added level 308 ikea to the city world

---
+++ src/com/poixson/backrooms/gens/Gen_308.java
@@ -0,0 +1,35 @@
+/*
+package com.poixson.backrooms.gens;
+
+import java.util.LinkedList;
+
+import org.bukkit.generator.ChunkGenerator.ChunkData;
+
+import com.poixson.backrooms.BackroomsGen;
+import com.poixson.backrooms.BackroomsLevel;
+import com.poixson.backrooms.PreGenData;
+import com.poixson.commonmc.tools.plotter.BlockPlotter;
+
+
+// 308 | Ikea
+public class Gen_308 extends BackroomsGen {
+
+
+
+	public Gen_308(final BackroomsLevel backlevel,
+			final int level_y, final int level_h) {
+		super(backlevel, level_y, level_h);
+	}
+
+
+
+	@Override
+	public void generate(final PreGenData pregen, final ChunkData chunk,
+			final LinkedList<BlockPlotter> plots, final int chunkX, final int chunkZ) {
+//TODO
+	}
+
+
+
+}
+*/
--- src/com/poixson/backrooms/worlds/Level_011.java
+++ src/com/poixson/backrooms/worlds/Level_011.java
@@ -8,16 +8,19 @@ import com.poixson.backrooms.dynmap.GeneratorTemplate;
 import com.poixson.commonmc.tools.plotter.BlockPlotter;
 
 
-// 11 | City
+//  11 | City
+// 308 | Ikea
 public class Level_011 extends BackroomsLevel {
 
 	public static final boolean ENABLE_GEN_011 = true;
 	public static final boolean ENABLE_TOP_011 = true;
 
-	public static final int LEVEL_Y = 0;
+	public static final int Y_011 = 0;
+	public static final int Y_308 = 0;
 
 	// generators
-	public final Gen_011 gen;
+	public final Gen_011 gen_011;
+	public final Gen_308 gen_308;
 
 
 
@@ -29,22 +32,41 @@ public class Level_011 extends BackroomsLevel {
 			gen_tpl.add(11, "city", "City");
 		}
 		// generators
-		this.gen = this.register(new Gen_011(this, LEVEL_Y, 0));
+		this.gen_011 = this.register(new Gen_011(this, Y_011, 0));
+		this.gen_308 = this.register(new Gen_308(this, Y_308, 0));
 	}
 
 
 
 	@Override
+	public int getLevelFromY(final int y) {
+		if (y < Y_011) return 308; // ikea
+		return 11;                 // city
+	}
+	@Override
 	public int getY(final int level) {
-		return LEVEL_Y;
+		switch (level) {
+		case 308: return Y_308; // ikea
+		case 11:  return Y_011; // city
+		default: throw new RuntimeException("Invalid backrooms level: "+Integer.toString(level));
+		}
 	}
 	@Override
 	public int getMaxY(final int level) {
-		return 255;
+		switch (level) {
+		case 308: return Y_011 - 1; // ikea
+		case 11:  return 320;       // city
+		default: throw new RuntimeException("Invalid backrooms level: "+Integer.toString(level));
+		}
 	}
 	@Override
 	public boolean containsLevel(final int level) {
-		return (level == 11);
+		switch (level) {
+		case 308: // ikea
+		case 11:  // city
+			return true;
+		default: return false;
+		}
 	}
 
 
@@ -52,7 +74,8 @@ public class Level_011 extends BackroomsLevel {
 	@Override
 	protected void generate(final int chunkX, final int chunkZ,
 			final ChunkData chunk, final LinkedList<BlockPlotter> plots) {
-		this.gen.generate(null, chunk, plots, chunkX, chunkZ);
+		this.gen_308.generate(null, chunk, plots, chunkX, chunkZ);
+		this.gen_011.generate(null, chunk, plots, chunkX, chunkZ);
 	}
 
 
