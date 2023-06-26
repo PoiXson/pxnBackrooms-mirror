f1b00624487783432b8c71ae56aa46d6a408dc67
lorenzop <lorenzo@poixson.com>
2023-06-26 00:52:47 -0400

    added water to valleys in level 94, and added ferns and wither roses

--- src/com/poixson/backrooms/gens/Gen_094.java
+++ src/com/poixson/backrooms/gens/Gen_094.java
@@ -5,8 +5,10 @@ import static com.poixson.backrooms.worlds.Level_094.ENABLE_GEN_094;
 import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.Map;
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.concurrent.atomic.AtomicReference;
 
+import org.bukkit.Material;
 import org.bukkit.block.data.BlockData;
 import org.bukkit.configuration.ConfigurationSection;
 import org.bukkit.configuration.file.FileConfiguration;
@@ -22,6 +24,7 @@ import com.poixson.tools.dao.Iab;
 import com.poixson.utils.FastNoiseLiteD;
 import com.poixson.utils.FastNoiseLiteD.FractalType;
 import com.poixson.utils.FastNoiseLiteD.NoiseType;
+import com.poixson.utils.RandomUtils;
 
 
 // 94 | Motion
@@ -31,12 +34,16 @@ public class Gen_094 extends BackroomsGen {
 	public static final double DEFAULT_VALLEY_DEPTH =  0.33;
 	public static final double DEFAULT_VALLEY_GAIN  =  0.3;
 	public static final double DEFAULT_HILLS_GAIN   = 12.0;
+	public static final int    DEFAULT_WATER_DEPTH  =  3;
+	public static final double DEFAULT_ROSE_CHANCE  =  0.01;
 
 	// default blocks
 	public static final String DEFAULT_BLOCK_DIRT        = "minecraft:dirt";
 	public static final String DEFAULT_BLOCK_GRASS_BLOCK = "minecraft:moss_block";
 	public static final String DEFAULT_BLOCK_GRASS_SLAB  = "minecraft:mud_brick_slab";
 	public static final String DEFAULT_BLOCK_GRASS       = "minecraft:grass";
+	public static final String DEFAULT_BLOCK_FERN        = "minecraft:fern";
+	public static final String DEFAULT_BLOCK_ROSE        = "minecraft:wither_rose";
 
 	// noise
 	public final FastNoiseLiteD noiseHills;
@@ -45,12 +52,16 @@ public class Gen_094 extends BackroomsGen {
 	public final AtomicDouble valley_depth = new AtomicDouble(DEFAULT_VALLEY_DEPTH);
 	public final AtomicDouble valley_gain  = new AtomicDouble(DEFAULT_VALLEY_GAIN );
 	public final AtomicDouble hills_gain   = new AtomicDouble(DEFAULT_HILLS_GAIN  );
+	public final AtomicInteger water_depth = new AtomicInteger(DEFAULT_WATER_DEPTH);
+	public final AtomicDouble rose_chance  = new AtomicDouble(DEFAULT_ROSE_CHANCE );
 
 	// blocks
 	public final AtomicReference<String> block_dirt        = new AtomicReference<String>(null);
 	public final AtomicReference<String> block_grass_block = new AtomicReference<String>(null);
 	public final AtomicReference<String> block_grass_slab  = new AtomicReference<String>(null);
 	public final AtomicReference<String> block_grass       = new AtomicReference<String>(null);
+	public final AtomicReference<String> block_fern        = new AtomicReference<String>(null);
+	public final AtomicReference<String> block_rose        = new AtomicReference<String>(null);
 
 
 
@@ -116,30 +127,49 @@ public class Gen_094 extends BackroomsGen {
 		final BlockData block_grass_block = StringToBlockData(this.block_grass_block, DEFAULT_BLOCK_GRASS_BLOCK);
 		final BlockData block_grass_slab  = StringToBlockData(this.block_grass_slab,  DEFAULT_BLOCK_GRASS_SLAB );
 		final BlockData block_grass       = StringToBlockData(this.block_grass,       DEFAULT_BLOCK_GRASS      );
+		final BlockData block_fern        = StringToBlockData(this.block_fern,        DEFAULT_BLOCK_FERN       );
+		final BlockData block_rose        = StringToBlockData(this.block_rose,        DEFAULT_BLOCK_ROSE       );
 		if (block_dirt        == null) throw new RuntimeException("Invalid block type for level 94 Dirt"      );
 		if (block_grass_block == null) throw new RuntimeException("Invalid block type for level 94 GrassBlock");
 		if (block_grass_slab  == null) throw new RuntimeException("Invalid block type for level 94 GrassSlab" );
 		if (block_grass       == null) throw new RuntimeException("Invalid block type for level 94 Grass"     );
+		if (block_fern        == null) throw new RuntimeException("Invalid block type for level 94 Fern"      );
+		final int depth_water = this.water_depth.get();;
+		final double wither_rose_chance = this.rose_chance.get();
 		final HashMap<Iab, HillsData> hillsData = ((PregenLevel94)pregen).hills;
 		HillsData dao;
 		final int y = this.level_y + 1;
 		int depth_dirt;
 		int mod_grass;
+		int rnd, rose_chance;
+		int last_rnd = 0;
 		for (int iz=0; iz<16; iz++) {
 			for (int ix=0; ix<16; ix++) {
+				chunk.setBlock(ix, this.level_y, iz, Material.BEDROCK);
 				dao = hillsData.get(new Iab(ix, iz));
 				// fill dirt
-				depth_dirt = (int) Math.floor(dao.depth - 0.7);
+				depth_dirt = (int) Math.floor(dao.depth - 1.7);
+				if (depth_dirt < 0) depth_dirt = 0;
 				for (int iy=0; iy<depth_dirt; iy++)
 					chunk.setBlock(ix, y+iy, iz, block_dirt);
-				// surface
+				// water
+				if (depth_dirt < depth_water) {
+					for (int iy=depth_dirt; iy<depth_water; iy++)
+						chunk.setBlock(ix, y+iy, iz, Material.WATER);
+				} else
+				// surface slab
 				if (dao.depth % 1.0 > 0.7) {
 					chunk.setBlock(ix, y+depth_dirt, iz, block_grass_slab);
+				// surface
 				} else {
 					chunk.setBlock(ix, y+depth_dirt, iz, block_grass_block);
 					mod_grass = (int)Math.floor(dao.valueHill * 1000.0) % 3;
-					if (mod_grass == 0)
-					chunk.setBlock(ix, y+depth_dirt+1, iz, block_grass);
+					rose_chance = (int) Math.round(1.0 / wither_rose_chance);
+					rnd = RandomUtils.GetNewRandom(0, rose_chance, last_rnd);
+					last_rnd += rnd;
+					if (rnd == 1)            chunk.setBlock(ix, y+depth_dirt+1, iz, block_rose );
+					else if (mod_grass == 0) chunk.setBlock(ix, y+depth_dirt+1, iz, block_grass);
+					else if (mod_grass == 1) chunk.setBlock(ix, y+depth_dirt+1, iz, block_fern );
 				}
 			}
 		}
@@ -160,6 +190,8 @@ public class Gen_094 extends BackroomsGen {
 			this.valley_depth.set(cfg.getDouble("ValleyDepth"));
 			this.valley_gain .set(cfg.getDouble("ValleyGain" ));
 			this.hills_gain  .set(cfg.getDouble("HillsGain"  ));
+			this.water_depth .set(cfg.getInt(   "WaterDepth" ));
+			this.rose_chance .set(cfg.getDouble("RoseChance" ));
 		}
 		// block types
 		{
@@ -168,16 +200,22 @@ public class Gen_094 extends BackroomsGen {
 			this.block_grass_block.set(cfg.getString("GrassBlock"));
 			this.block_grass_slab .set(cfg.getString("GrassSlab" ));
 			this.block_grass      .set(cfg.getString("Grass"     ));
+			this.block_fern       .set(cfg.getString("Fern"      ));
+			this.block_rose       .set(cfg.getString("Rose"      ));
 		}
 	}
 	public static void ConfigDefaults(final FileConfiguration cfg) {
 		cfg.addDefault("Level94.Params.ValleyDepth", DEFAULT_VALLEY_DEPTH);
 		cfg.addDefault("Level94.Params.ValleyGain",  DEFAULT_VALLEY_GAIN );
 		cfg.addDefault("Level94.Params.HillsGain",   DEFAULT_HILLS_GAIN  );
+		cfg.addDefault("Level94.Params.WaterDepth",  DEFAULT_WATER_DEPTH );
+		cfg.addDefault("Level94.Params.RoseChance",  DEFAULT_ROSE_CHANCE );
 		cfg.addDefault("Level94.Blocks.Dirt",       DEFAULT_BLOCK_DIRT       );
 		cfg.addDefault("Level94.Blocks.GrassBlock", DEFAULT_BLOCK_GRASS_BLOCK);
 		cfg.addDefault("Level94.Blocks.GrassSlab",  DEFAULT_BLOCK_GRASS_SLAB );
 		cfg.addDefault("Level94.Blocks.Grass",      DEFAULT_BLOCK_GRASS      );
+		cfg.addDefault("Level94.Blocks.Fern",       DEFAULT_BLOCK_FERN       );
+		cfg.addDefault("Level94.Blocks.Rose",       DEFAULT_BLOCK_ROSE       );
 	}
 
 
